77dc4053ef9a1306d8722beef17e0a7e3fa469bb
ncyamane <chabajunsei@gmail.com>
2019-11-05 15:37:41 +0900

    Reinitial Commit

--- main.cpp
+++ main.cpp
@@ -1,31 +1,19 @@
-
-// Function to show an array
-void array2D(const std::vector<std::vector<size_t>>& array_2d) {
-for (size_t i = 0; i < array_2d.size(); ++i) {
-    for (size_t j = 0; j < array_2d[i].size(); ++j) {
-        //配列の中身を出力
-        std::cout << array_2d[i][j] << ',';
-    }
-    //改行
-    std::cout << std::endl;
-    }
-}
-
-int main(){
-
-    // Declaration of 2-dim array
-    std::vector<std::vector<size_t>> a(3, std::vector<size_t>(3, 1.0));
-    std::vector<std::vector<size_t>> aa(3, std::vector<size_t>(3));
-    std::vector<std::vector<size_t>> m(3, std::vector<size_t>(3));
-
-    for (size_t i = 0; i < array_2d.size(); ++i) {
-        for (size_t j = 0; j < array_2d[i].size(); ++j) {
-        
-        m[]
-    }
-    // show an array
-    array2D(a);
-
-    return 0;
-
+int main(int argc, char const *argv[]) {
+  std::chrono::system_clock::time_point start, end;
+  double elapsed;
+  const long int MAX = 100000;
+  long int a[MAX+1];
+  // Array
+  start = std::chrono::system_clock::now();
+  for (long int i = 0; i <= MAX; i++) {
+    a[i] = i;
+  }
+  long int sum1 = 0;
+  for (long int i = 0; i <= MAX; i++) {
+    sum1 += a[i];
+  }
+  end = std::chrono::system_clock::now();
+  elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
+  std::cout << elapsed << "μs" << std::endl;
+  return 0;
 }
