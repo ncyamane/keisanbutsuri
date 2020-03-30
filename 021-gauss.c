1c53b7a17ab3525bd8c5f10c340dd94a34469ba6
ncyamane <chabajunsei@gmail.com>
2020-03-31 00:23:43 +0900

    ちょい修正

--- 021-gauss.c
+++ 021-gauss.c
@@ -3,49 +3,47 @@
 #define DIM 3
 
 void Printmatrix(uint32_t row_size, uint32_t column_size, double *matrix) {
-  for (uint32_t i = 0; i < row_size; ++i) {
-    for (uint32_t j = 0; j < column_size; ++j) {
-      printf("%lf ", matrix[i * row_size + j]);
+    for (uint32_t i = 0; i < row_size; ++i) {
+        for (uint32_t j = 0; j < column_size; ++j) {
+            printf("%lf ", matrix[i * row_size + j]);
+        }
+        printf("\n");
     }
-    printf("\n");
-  }
 }
 
 int main() {
-  double mat_a[DIM * DIM] = {2, -3, -1, 3, -2, 2, 1, -2, 1};
-  Printmatrix(DIM, DIM, mat_a);
+    double mat_a[DIM * DIM] = {2, -3, -1, 3, -2, 2, 1, -2, 1};
+    Printmatrix(DIM, DIM, mat_a);
 
-  double vec_b[DIM] = {1, -3, -4};
+    double vec_b[DIM] = {1, -3, -4};
 
-  // Choose the pivot
-  for (uint32_t i = 0; i < DIM; i++) {
-    printf("Pivot\n");
-  }
-
-  for (uint32_t i = 0; i < DIM; ++i) {
-    for (uint32_t j = i + 1; j < DIM; ++j) {
-      double c = mat_a[j * DIM + i] / mat_a[i * DIM + i];
-      for (uint32_t k = i; k < DIM; ++k) {
-        B[j * n + k] = B[j * n + k] - c * B[i * n + k];
-      }
+    for (uint32_t i = 0; i < DIM; ++i) {
+        // Choose the pivot
+        printf("Pivot\n");
+        for (uint32_t j = i + 1; j < DIM; ++j) {
+            double pivot = mat_a[j * DIM + i] / mat_a[i * DIM + i];
+            for (uint32_t k = i; k < DIM; ++k) {
+                mat_a[j * DIM + k] -= mat_a[i * DIM + k] * pivot;
+                vec_b[k] -= vec_b[k] * pivot;
+            }
+        }
     }
-  }
 
-  Printmatrix(DIM, DIM, mat_a);
-  printf("\n");
-  Printmatrix(DIM, 1, vec_b);
-  printf("\n");
+    Printmatrix(DIM, DIM, mat_a);
+    printf("\n");
+    Printmatrix(DIM, 1, vec_b);
+    printf("\n");
 
-  double x[3] = {0};
-  for (uint32_t i = n - 1; i >= 0; --i) {
-    for (uint32_t j = i + 1; j < n; ++j) {
-      x[i] -= B[i * n + j] * x[j];
+    double x[3] = {0};
+    for (uint32_t i = DIM - 1; i >= 0; --i) {
+        for (uint32_t j = i + 1; j < DIM; ++j) {
+            x[i] -= mat_a[i * DIM + j] * x[j];
+        }
+        x[i] += mat_a[i * DIM + DIM];
+        x[i] /= mat_a[i * DIM + i];
     }
-    x[i] += B[i * n + n];
-    x[i] /= B[i * n + i];
-  }
 
-  for (uint32_t i = 0; i < n; ++i) {
-    printf("%lf ", x[i]);
-  }
+    for (uint32_t i = 0; i < DIM; ++i) {
+        printf("%lf ", x[i]);
+    }
 }
